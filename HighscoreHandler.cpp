a0c1dbacedbdce86efff4de3f64fec1f4d60ec6f
Nico <nicbritos@hotmail.com>
2019-06-09 20:19:26 -0300

    Fixed some pointer arithmetic related bugs and commented HighscoreHandler

--- HighscoreHandler.cpp
+++ HighscoreHandler.cpp
@@ -1,66 +1,84 @@
 #include "HighscoreHandler.h"
 
-#define BYTE_SIZE 8
 #define BYTE_MASK 0xFF
-#define END_BYTE -1
+#define BYTE_SIZE 8
+#define END_BYTE -1LL // Long long para evitar problemas con operadores bitwise
 #define CHUNK 5
 
-int8_t descendingCompareFunction(const void *a, const void *b);
-
-HighscoreHandler::HighscoreHandler(uint32_t _startingAddress, uint32_t _maxScores) {
-  startingAddress = _startingAddress;
-  maxScores = _maxScores;
-  scores = NULL;
-  load();
+// Funcion de comparacion utilizada para ordenacion (descendente) en RAM de scores
+int8_t descendingCompareFunction(const void *a, const void *b) {
+  int64_t result = *((uint64_t*) b) - *((uint64_t*) a);
+  return result > 0 ? 1 : result < 0 ? -1 : result;
 }
 
+
+// ---- Private ----
 void HighscoreHandler::registerScoreEEPROM(uint64_t score, uint64_t scoreToRemove) {
   uint32_t address = findEEPROMAddress(scoreToRemove);
-  write64(address, score);
+  
+  // Pisa valor anterior
+  writeEEPROM(address, score);
 }
 
-void HighscoreHandler::write64(uint32_t address, uint64_t data) {
-  // MSB first
-  for (uint32_t i = 0; i < sizeof(data); i++) {
-    if (EEPROM.read(address + i) != (data & BYTE_MASK)) {
-      EEPROM.write(address + i, data & BYTE_MASK);
+void HighscoreHandler::writeEEPROM(uint32_t address, uint64_t data) {
+  // Guarda LSB primero (similar a como lo realiza un sistema Little Endian)
+  // Uso de sizeof permite no dejar espacios sin escribir en ROM en sistemas que
+  // no soportan enteros de 64 bits.
+  for (uint8_t i = 0; i < sizeof(data); i++) {
+    // Nos quedamos con el byte menos significativo
+    uint8_t b = data & BYTE_MASK;
+    
+    // Debido a que la cantidad de escrituras en las memorias EEPROM suele ser
+    // limitada (entre 10.000 y 100.000 escrituras para un Arduino UNO (Atmega 328p))
+    // y suele tardar un tiempo considerable (3.3ms para el dispositivo recien 
+    // mencionado), primero confirmamos que no sobreescribiremos datos que ya se
+    // encuentran guardados.
+    if (EEPROM.read(address + i) != b) {
+      EEPROM.write(address + i, b);
     }
+    
+    // Shifteamos a la derecha un byte (MSB a LSB) para procesarlo en la proxima
+    // iteracion
     data >>= BYTE_SIZE;
   }
 }
 
-uint32_t HighscoreHandler::findEEPROMAddress(uint64_t score) {
-  uint32_t i, latest, current;
-  
-  for (current = 0, latest = i = startingAddress; current < count; current++) {
-    uint64_t romScore = read64(i);
-    
-    if (romScore == score) {
-      latest = i;
-    }
-    i += 4;
-  }
-  
-  return latest;
-}
-
-uint32_t HighscoreHandler::findPosition(uint64_t score) {
+uint32_t HighscoreHandler::findPositionInScores(uint64_t score) {
   uint32_t ix;
   
-  // Check for eq to make sure that, if the same score is registered,
-  // then the new rank (or position) is not the first one (preserve order)
+  // Chequeo de igualdad con el fin de mantener estabilidad en orden de llegada
   for (ix = 0; ix < count && scores[ix] >= score; ix++);
   
   return ix;
 }
 
-uint64_t HighscoreHandler::read64(uint32_t address) {
+uint32_t HighscoreHandler::findEEPROMAddress(uint64_t score) {
+  // Asume que score existe y esta en la EEPROM! (Y las funciones que la llaman
+  // se aseguran de esto pues solo se busca en EEPROM para eliminar). Notemos 
+  // que el retorno de parametro es mas sencillo de esta manera
+  uint32_t i, current;
+  
+  for (current = 0, i = startingAddress; current < count; current++) {
+    uint64_t romScore = readEEPROM(i);
+    
+    if (romScore == score) {
+      break;
+    }
+    
+    i += sizeof(score) ;
+  }
+  
+  return i;
+}
+
+uint64_t HighscoreHandler::readEEPROM(uint32_t address) {
   uint64_t ret = 0;
   
-  // MSB first
-  for (uint32_t i = 0; i < sizeof(ret); i++) {
+  // Little Endian
+  for (uint8_t i = 0; i < sizeof(ret); i++) {
     ret |= EEPROM.read(address + i);
     
+    // Si no chequeamos eliminamos el MSB!
     if (i != sizeof(ret) - 1) {
       ret <<= BYTE_SIZE;
     }
@@ -70,50 +88,54 @@ uint64_t HighscoreHandler::read64(uint32_t address) {
 }
 
 void HighscoreHandler::shiftScores(uint32_t from) {
-  if (from >= count)
-    return; // Nothing to shift.
+  // from invalido o count esta en la ultima posicion (y, por lo tanto, no
+  // hay que shiftear el array)
+  if (from >= count - 1)
+    return;
   
-  // Avoid buffer overflow
-  if (count < currentSize) {
+  // Evita buffer overflow
+  // Si hay espacio disponible copia el ultimo valor. En caso contrario,
+  // se pisara adelante (si es que ya se alcanzo el numero maximo de 
+  // scores a almacenar)
+  if (count < currentLength) {
     scores[count] = scores[count - 1];
   }
   
-  for (uint32_t i = count - 2; i >= from; i--) {
-    scores[i + 1] = scores[i];
+  for (uint32_t i = count - 2; i > from; i--) {
+    scores[i] = scores[i - 1];
   }
 }
 
 bool HighscoreHandler::endReached(uint64_t data) {
-  return data == END_BYTE;
-}
-
-void HighscoreHandler::initEEPROM() {
-  count = 0;
-  write64(startingAddress, END_BYTE);
+  // Chequea que el bit de signo (el MSb) este prendido, indicando si es
+  // o no el final del array almacenado en la EEPROM.
+  return data >> (sizeof(data) * BYTE_SIZE - 1);
 }
 
 void HighscoreHandler::load() {
   if (scores != NULL) {
     free(scores);
     scores = NULL;
+    currentLength = count = 0;
   }
   
   if (maxScores <= CHUNK) {
-    currentSize = maxScores;
-    scores = (uint64_t*) malloc(currentSize * sizeof(*scores));
+    currentLength = maxScores;
+    scores = (uint64_t*) malloc(currentLength * sizeof(*scores));
   }
   
   uint64_t address, data;
   for (count = 0; count < maxScores; count++) {
     address = startingAddress + (count * sizeof(*scores));
-    data = read64(address);
+    data = readEEPROM(address);
     
     if (endReached(data))
       break;
     
-    if (maxScores > CHUNK && count > 0 && count % CHUNK == 0) {
-      currentSize = count + CHUNK;
-      scores = (uint64_t*) realloc(scores, currentSize * sizeof(*scores));
+    // Agranda el vector si es necesario
+    if (maxScores > CHUNK && count % CHUNK == 0) {
+      currentLength = count + CHUNK;
+      scores = (uint64_t*) realloc(scores, currentLength * sizeof(*scores));
     }
     
     scores[count] = data;
@@ -123,39 +145,61 @@ void HighscoreHandler::load() {
 }
 
 
+// ---- Public ----
+HighscoreHandler::HighscoreHandler(uint32_t _startingAddress, uint32_t _maxScores) {
+  startingAddress = _startingAddress;
+  maxScores = _maxScores;
+  currentLength = 0;
+  scores = NULL;
+  count = 0;
+  load();
+}
 
 uint32_t HighscoreHandler::registerScore(uint64_t score){
-  if (count < maxScores && count + 1 == currentSize) {
-    if (count + 1 + CHUNK >= maxScores) {
-      currentSize = maxScores;
-    } else {
-      currentSize = count + 1 + CHUNK;
-    }
-    
-    scores = (uint64_t*) realloc(scores, currentSize * sizeof(*scores));
+  // Guardamos el ultimo elemento (que, como el array se encuentra ordenado de manera
+  // descendente, es el numero mas chico o el ultimo por orden de llegada si coincide
+  // con el inmediato anterior) para luego poder eliminarlo de la EEPROM si es necesario.
+  uint64_t lastElement = scores[count-1];
+  if (score <= lastElement) {
+    return 0;
   }
   
-  uint64_t lastElement = scores[count-1];
+  // Si es posible agrandar el array (no se alcanzo a registrar la cantidad
+  // maxima de scores) y va a ser necesario luego de agregar el score, lo agrandamos
+  if (count < maxScores && count + 1 == currentLength) {
+    if (count + 1 + CHUNK >= maxScores) {
+      currentLength = maxScores;
+    } else {
+      currentLength = count + 1 + CHUNK;
+    }
+    
+    scores = (uint64_t*) realloc(scores, currentLength * sizeof(*scores));
+  }
   
-  uint32_t ix = findPosition(score);
+  // Buscamos posicion de insercion en el array, lo movemos (para hacer un lugar)
+  // y lo almacenamos
+  uint32_t ix = findPositionInScores(score);
   shiftScores(ix);
   scores[ix] = score;
   
   if (count < maxScores) {
+    // Como no se alcanzo la cantidad maxima de scores, podemos guardarlo en EEPROM
+    // en la primera direccion disponible desde el offset "startingAddress"
     count++;
-    write64(startingAddress + (count * sizeof(count)), score);
+    writeEEPROM(startingAddress + (count * sizeof(score)), score);
   } else {
+    // Pisamos valor previamente eliminado por el nuevo score.
     registerScoreEEPROM(score, lastElement);
   }
   
-  return ix;
+  return ix + 1; // Ranking empieza en 1
 }
 
 uint32_t HighscoreHandler::getScoresAmmount() {
   return count;
 }
 
-uint64_t* HighscoreHandler::getScores() {
+const uint64_t* HighscoreHandler::getScores() {
   return scores;
 }
 
@@ -163,14 +207,9 @@ void HighscoreHandler::reset() {
   if (scores != NULL) {
     free(scores);
     scores = NULL;
+    currentLength = count = 0;
   }
 
-  initEEPROM();
+  writeEEPROM(startingAddress, END_BYTE);
   load();
-}
-
-
-int8_t descendingCompareFunction(const void *a, const void *b) {
-  int64_t result = *((uint64_t*) b) - *((uint64_t*) a);
-  return result > 0L ? 1L : result < 0L ? -1L : result;
 }
--- HighscoreHandler.h
+++ HighscoreHandler.h
@@ -7,27 +7,60 @@
 
 class HighscoreHandler {
   private:
+    // ---- Variables de instancia ----
+    // Direccion de comienzo de EEPROM
     uint32_t startingAddress;
-    uint8_t currentSize;
-    uint32_t maxScores;
-    uint64_t* scores;
-    uint8_t count;
     
+    // Almacena el tamano real del vector de scores
+    uint32_t currentLength;
+    uint32_t maxScores;
+    
+    // Puntero a primer score
+    uint64_t* scores;
+    
+    // Cantidad efectiva de scores almacenados
+    uint32_t count;
+    
+    
+    // ---- Metodos de instancia ----
     void registerScoreEEPROM(uint64_t score, uint64_t scoreToRemove);
-    void write64(uint32_t address, uint64_t data);
+    void writeEEPROM(uint32_t address, uint64_t data);
+    
+    // Retorna indice donde insertar score en el array.
+    uint32_t findPositionInScores(uint64_t score);
     uint32_t findEEPROMAddress(uint64_t score);
-    uint32_t findPosition(uint64_t score);
-    uint64_t read64(uint32_t address);
+    
+    // Lee 64 bits de EEPROM
+    uint64_t readEEPROM(uint32_t address);
+    
+    // Desplaza los scores en el array desde la posicion from copiandolos
+    // desde este indice hasta el final del array + 1
     void shiftScores(uint32_t from);
+    
+    // Comprueba si el dato levantado de EEPROM es de finalizacion (< 0)
     bool endReached(uint64_t data);
-    void initEEPROM();
+    
+    // Lee los scores guardados en EEPROM y los ordena (en RAM) al finalizar
     void load();
     
   public:
+    // "_startingAddress" tiene que ser una direccion de EEPROM valida (dependiente de board).
     HighscoreHandler(uint32_t _startingAddress, uint32_t _maxScores);
+    
+    // Registra un score y la guarda tanto en RAM (ordenado descendentemente) 
+    // como en EEPROM (sin ordenar). El algoritmo utilizado para ordenacion (en RAM) 
+    // es estable, por lo que, bajo el mismo score, mantiene el orden de llegada.
+    // Devuelve el ranking del score si es un nuevo highscore, o 0 en caso contrario.
     uint32_t registerScore(uint64_t score);
+    
+    // Devuelve la cantidad de scores almacenado.
     uint32_t getScoresAmmount();
-    uint64_t* getScores();
+    
+    // Devuelve un puntero a la direccion del primer score.
+    const uint64_t* getScores();
+    
+    // Resetea zona de EEPROM utilizada para almacenaje de scores.
+    // Setea un byte especial en la direccion de memoria "startingAddress"
     void reset();
 };
 
