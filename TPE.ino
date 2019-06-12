d27ef96c9f5a83d7d045aaa7377a6a3c2b58f419
AgustinRoca <40873168+AgustinRoca@users.noreply.github.com>
2019-06-12 20:18:23 -0300

    Para 4 matrices

--- TPE.ino
+++ TPE.ino
@@ -11,8 +11,8 @@
 #define MATRIX_ROWS 8 // Cantidad de filas de LEDs que tiene UNA matriz
 #define MATRIX_SIZE (MATRIX_ROWS * MATRIX_COLUMNS) // Cantidad total de LEDs que tiene UNA matriz
 #define MAX_LENGTH (MATRIX_SIZE * VERTICAL_MATRIXES_QTY * HORIZONTAL_MATRIXES_QTY) // Cantidad total de LEDs totals que tiene la pantalla completa (la vibora no puede ser mas grande que eso tampoco)
-#define HORIZONTAL_MATRIXES_QTY 1
-#define VERTICAL_MATRIXES_QTY 1
+#define HORIZONTAL_MATRIXES_QTY 2
+#define VERTICAL_MATRIXES_QTY 2
 
 /* Constantes que definen la intensidad de un LED para que se considere prendido o apagado */
 #define ON 8 //intensidad de led cuando se prende
@@ -40,11 +40,18 @@
 #define CS_PIN 13 // Pin que se conecta al CS de la matriz
 #define DIFICULTY_INTERVAL 0.3
 
+/* Limpia el serial */
+void cleanSerial(){
+  while(Serial.available()){
+    Serial.read();
+  }
+}
+
 /* Traduce el boton apretado y devuelve la direccion en la que deberia seguir la vibora en base a ese boton, si no hubo boton apretado, sigue en la misma direccion que estaba */
-Direction translateInput(Direction currentDir, HighscoreHandler * highscore, InputHandler input, MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY], int * intensity){
+Direction translateInput(Direction currentDir, HighscoreHandler * highscore, InputHandler input, MaxMatrix screen, int * intensity){
   if(Serial.available()){
     char c = Serial.read(); //Deberiamos agregar un while(Serial.available()) para que lea todo el buffer por si se insertaron mas de una letra? Nos quedamos con la primera o la ultima?
-    
+    cleanSerial();
     switch(toupper(c)){
       case 'A': // Flecha izquierda
         return currentDir = (Direction) ((4 + currentDir - 1) % 4); // Giro en sentido antihorario (sumo 4 para que quede positivo, para que el % me de positivo)
@@ -58,7 +65,7 @@ Direction translateInput(Direction currentDir, HighscoreHandler * highscore, Inp
         return currentDir;
       case 'I':
         *intensity = Serial.read() - '0';
-        screen[0][0].setIntensity(*intensity);
+        screen.setIntensity(*intensity);
         return currentDir;
       default:
         return currentDir;
@@ -80,61 +87,109 @@ void printHighscores(HighscoreHandler highscore) {
   } else {
     Serial.println("No hay puntajes registrados");
   }
+  Serial.println("--------------------------------------------");
 }
 
 /* Prende todo el array de body en las matrices (hacer un clear antes de esto) */
-void printWholeBody(Position body[MAX_LENGTH], int currentLength, int head, MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY], int intensity){
+void printWholeBody(Position body[MAX_LENGTH], int currentLength, int head, MaxMatrix * screen, int intensity){
   for(int i=0; i<currentLength; i++){
-    screen[0][0].setDot(body[(MAX_LENGTH + head-i) % MAX_LENGTH].y, body[(MAX_LENGTH + head-i) % MAX_LENGTH].x, intensity); //Sumo MAX_LENGTH para que el indice quede positivo
+    if(body[(MAX_LENGTH + head-i)%MAX_LENGTH].x < MATRIX_COLUMNS){ // Matrices izquierdas
+      if(body[(MAX_LENGTH + head-i)%MAX_LENGTH].y < MATRIX_ROWS){ // Matriz abajo-izquierda (1era matriz de la cascada)
+        screen->setDot(7-body[(MAX_LENGTH + head-i)%MAX_LENGTH].y, 7-body[(MAX_LENGTH + head-i)%MAX_LENGTH].x, intensity);
+      }
+      else{ // Matriz arriba-izquierda (3era matriz de la cascada)
+        screen->setDot(7-body[(MAX_LENGTH + head-i)%MAX_LENGTH].y, (7-(body[(MAX_LENGTH + head-i)%MAX_LENGTH].x % MATRIX_COLUMNS)) + 2*MATRIX_COLUMNS, intensity);
+      }
+    }
+    else{ // Matrices Derechas
+      if(body[(MAX_LENGTH + head-i)%MAX_LENGTH].y < MATRIX_ROWS){ // Matriz abajo-derecha (2da matriz de la cascada) ESTA INVERTIDA 
+        screen->setDot(body[(MAX_LENGTH + head-i)%MAX_LENGTH].y, (body[(MAX_LENGTH + head-i)%MAX_LENGTH].x % MATRIX_COLUMNS) + MATRIX_COLUMNS, intensity);
+      }
+      else{ // Matriz arriba-derecha (4ta matriz de la cascada) ESTA INVERTIDA
+        screen->setDot(body[(MAX_LENGTH + head-i)%MAX_LENGTH].y, (body[(MAX_LENGTH + head-i)%MAX_LENGTH].x % MATRIX_COLUMNS) + 3*MATRIX_COLUMNS, intensity);
+      }
+    }
   }
 }
 
 /* Prende el LED de la nueva cabeza de la vibora, apaga el LED de la vieja cola de la snake */
-void printMove(Position newHead, Position oldTail, MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY], int intensity){
-  screen[0][0].setDot(oldTail.y, oldTail.x, OFF);
-  screen[0][0].setDot(newHead.y, newHead.x, intensity);
-}
+void printMove(Position newHead, Position oldTail, MaxMatrix * screen, int intensity){
+  if(oldTail.x < MATRIX_COLUMNS){ // Matrices izquierdas
+    if(oldTail.y < MATRIX_ROWS){ // Matriz abajo-izquierda (1era matriz de la cascada)
+      screen->setDot(7-oldTail.y, 7-oldTail.x, OFF);
+    }
+    else{ // Matriz arriba-izquierda (3era matriz de la cascada)
+      screen->setDot(7-oldTail.y, ((7-oldTail.x) % MATRIX_COLUMNS) + 2*MATRIX_COLUMNS, OFF);
+    }
+  }
+  else{ // Matrices Derechas
+    if(oldTail.y < MATRIX_ROWS){ // Matriz abajo-derecha (2da matriz de la cascada) ESTA INVERTIDA 
+      screen->setDot(oldTail.y, (oldTail.x % MATRIX_COLUMNS) + MATRIX_COLUMNS, OFF);
+    }
+    else{ // Matriz arriba-derecha (4ta matriz de la cascada) ESTA INVERTIDA
+      screen->setDot(oldTail.y, ((oldTail.x) % MATRIX_COLUMNS) + 3*MATRIX_COLUMNS, OFF);
+    }
+  }
 
-/* Imprime la cruz cuando se pierde */
-void printSkull(MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY]){
-  byte sf[8]= {B00111100,B01000010,B10100101,B10011001,B10011001,B10100101,B01000010,B00111100};
-  for(int i=0; i<MATRIX_COLUMNS * HORIZONTAL_MATRIXES_QTY; i++){
-    screen[0][0].setColumn(7-i, sf[i]);
+  
+  if(newHead.x < MATRIX_COLUMNS){ // Matrices izquierdas
+    if(newHead.y < MATRIX_ROWS){ // Matriz abajo-izquierda (1era matriz de la cascada)
+      screen->setDot(7-newHead.y, 7-newHead.x, intensity);
+    }
+    else{ // Matriz arriba-izquierda (3era matriz de la cascada)
+      screen->setDot(7-newHead.y, (7-(newHead.x % MATRIX_COLUMNS)) + 2*MATRIX_COLUMNS, intensity);
+    }
+  }
+  else{ // Matrices Derechas
+    if(newHead.y < MATRIX_ROWS){ // Matriz abajo-derecha (2da matriz de la cascada) ESTA INVERTIDA 
+      screen->setDot(newHead.y, (newHead.x % MATRIX_COLUMNS) + MATRIX_COLUMNS, intensity);
+    }
+    else{ // Matriz arriba-derecha (4ta matriz de la cascada) ESTA INVERTIDA
+      screen->setDot(newHead.y, (newHead.x % MATRIX_COLUMNS) + 3*MATRIX_COLUMNS, intensity);
+    }
   }
 }
 
-void printMenu(HighscoreHandler * highscore,  MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY],int * intensity){
-  Serial.println("SNAKE\n");
+/* Imprime la cruz cuando se pierde */
+void printSkull(MaxMatrix * screen){
+  byte sf[8]= {B00111100,B01000010,B10100101,B10011001,B10011001,B10100101,B01000010,B00111100};
+  for(int i=0; i<MATRIX_COLUMNS * HORIZONTAL_MATRIXES_QTY; i++){
+    screen->setColumn(7-i, sf[i]);
+  }
+}
+
+void printMenu(HighscoreHandler * highscore, int * intensity){
+  Serial.println("\nSNAKE\n");
   Serial.println("1. Play");
   Serial.println("2. Show Highscores");
   Serial.println("3. Reset Highscores");
   Serial.println("4x. Set Intensity in x (x = (1..8))");
   Serial.println("5x. Dificulty in x ( x = 1, 2 or 3)");
+  Serial.println("--------------------------------------------");
 }
 
-void readMenuInput(Snake * snake, HighscoreHandler * highscore,  MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY],int * intensity, uint64_t * lastUpdatedMillis, uint64_t * lastMovedMillis, Direction * input, double * waitTimeFactor, double * waitDecreaseRatioFactor){
+void readMenuInput(Snake * snake, HighscoreHandler * highscore,  MaxMatrix * screen,int * intensity, uint64_t * lastUpdatedMillis, uint64_t * lastMovedMillis, Direction * input, double * waitTimeFactor, double * waitDecreaseRatioFactor){
   if(Serial.available()){
     char c = Serial.read(); //Deberiamos agregar un while(Serial.available()) para que lea todo el buffer por si se insertaron mas de una letra? Nos quedamos con la primera o la ultima?
     
     switch(toupper(c)){
       case '1': // Play
-        snake->revive( INIT_LENGTH, INIT_DIR, INIT_WAIT, INIT_ROW_POS, INIT_COL_POS);
-        screen[0][0].clear();
+        snake->revive(INIT_LENGTH, INIT_DIR, INIT_WAIT, INIT_ROW_POS, INIT_COL_POS);
+        screen->clear();
         printWholeBody(snake->getBody(), snake->getCurrentLength(), snake->getHead(), screen, *intensity);
         *lastUpdatedMillis = *lastMovedMillis = millis();
         *input = INIT_DIR;
       break;
       case '2': // Show Highscores
-        //system("clear");
         printHighscores(*highscore);
-        delay(5000);
+        delay(2000);
       break;
       case '3': // Reset Highscores
         highscore->resetScores();
         break;
       case '4': // Set intensity
         *intensity = Serial.read() - '0';
-        screen[0][0].setIntensity(*intensity);
+        screen->setIntensity(*intensity);
         break;
       case '5': // Dificulty
         *waitDecreaseRatioFactor = *waitTimeFactor = 1;
@@ -145,15 +200,14 @@ void readMenuInput(Snake * snake, HighscoreHandler * highscore,  MaxMatrix scree
       break;
     }
     if(toupper(c) != '1' && c!='\r'){
-      //system("clear");
-      printMenu(highscore, screen, intensity);
+      printMenu(highscore, intensity);
     }
   }
 }
 
 /* Creacion de variables globales */
 Snake snake(INIT_LENGTH, INIT_DIR, INIT_WAIT, INIT_ROW_POS, INIT_COL_POS, HORIZONTAL_MATRIXES_QTY * MATRIX_COLUMNS, VERTICAL_MATRIXES_QTY * MATRIX_ROWS); // La parte logica de la viborita
-MaxMatrix screen[VERTICAL_MATRIXES_QTY][HORIZONTAL_MATRIXES_QTY] = {{MaxMatrix(DATA_PIN,CS_PIN,CLK_PIN,1)}}; //{{MaxMatrix(1,2,3,4), MaxMatrix(5,6,7,8)}, {MaxMatrix(11,13,10,1), MaxMatrix(13,14,15,0)}}; //0,0 = Arriba izquierda; 0,1 = Arriba derecha; 1,0 = Abajo izquierda; 1,1 = Arriba derecha
+MaxMatrix screen(DATA_PIN,CS_PIN,CLK_PIN,1);  //0,0 = Arriba izquierda; 0,1 = Arriba derecha; 1,0 = Abajo izquierda; 1,1 = Arriba derecha
 Direction input = INIT_DIR; // La direccion que empieza la vibora (lo inicializo en eso porque si no hay boton devuelve eso)
 uint64_t lastMovedMillis = 0; // El tiempo (en ms) en el que se movio la vibora la ultima vez
 uint64_t lastUpdatedMillis = 0; // El tiempo (en ms) en los que se agrando la vibora la ultima vez
@@ -172,17 +226,13 @@ void setup() {
   Serial.begin(115200);
   
   //Inicializacion de las matrices de LEDs
-  for(int i=0; i<VERTICAL_MATRIXES_QTY; i++){
-    for(int j=0; j<HORIZONTAL_MATRIXES_QTY; j++){
-      screen[i][j].init();
-      screen[i][j].setIntensity(intensity);
-      screen[i][j].clear();
-    }
-  }
+  screen.init();
+  screen.setIntensity(intensity);
+  screen.clear();
 
   highscore = HighscoreHandler(INITIAL_EEPROM_ADDRESS, MAX_HIGHSCORES);
   lastUpdatedMillis = lastMovedMillis = millis();
-  printMenu(&highscore, screen, &intensity);
+  printMenu(&highscore, &intensity);
 }
 
 void loop() {
@@ -204,13 +254,13 @@ void loop() {
         snake.moveSnake(input, enlarge);
       
         if(snake.isAlive()) // Si sigue viva despues de ese movimiento
-          printMove(snake.getBody()[snake.getHead()], oldTail, screen, intensity); // Actualizo la pantalla
+          printMove(snake.getBody()[snake.getHead()], oldTail, &screen, intensity); // Actualizo la pantalla
         else{
-          printSkull(screen); // Imprimo la pantalla de GAME OVER
+          printSkull(&screen); // Imprimo la pantalla de GAME OVER
           snake.freeSnake(); // Libera solo el body, no es un destructor
           highscore.registerScore(snake.getAliveTime()); // Si fue un highscore, lo guarda
           delay(1000);
-          printMenu(&highscore, screen, &intensity);
+          printMenu(&highscore, &intensity);
         }
         enlarge = false; // Si lo agrandaba, ya no lo tengo que agrandar
         
@@ -221,7 +271,7 @@ void loop() {
     }
   }
   else{
-    readMenuInput(&snake, &highscore, screen, &intensity, &lastUpdatedMillis, &lastMovedMillis, &input, &waitTimeFactor, &waitDecreaseRatioFactor);
+    readMenuInput(&snake, &highscore, &screen, &intensity, &lastUpdatedMillis, &lastMovedMillis, &input, &waitTimeFactor, &waitDecreaseRatioFactor);
   }
 }
   
